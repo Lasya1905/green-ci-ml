ae10837dba0b805efb4f5d8e5fa1643345223f71
Lasya1905 <23951a055u@iare.ac.in>
2025-12-13 15:55:00 +0530

    update app and tests

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
     return "Hello from Energy-Aware CI/CD Project!"
 
+@app.route('/compute')
+def compute():
+    x = 0
+    for i in range(500000):
+        x += i
+    return str(x)
+
+
 if __name__ == '__main__':
     app.run()
--- test_app.py
+++ test_app.py
@@ -3,3 +3,9 @@ import time
 def test_dummy():
     time.sleep(3)
     assert True
+
+def test_loop():
+    x = 0
+    for i in range(100000):
+        x += i
+    assert x > 0
